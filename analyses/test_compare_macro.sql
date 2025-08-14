8bc15ffd1d4c32bbdc96278713863f49bfffe152
Chuck Frisbie <chuck.frisbie@fishtownanalytics.com>
2025-08-14 21:46:48 +0000

    Phase 1 (manual) complete for now

---
+++ analyses/test_compare_macro.sql
@@ -0,0 +1,7 @@
+{{ get_model_name_mismatches_sql(
+    database='FRISBIE_DEVELOPMENT',
+    schema='SANDBOX',
+    table_name='SNOW_SV_LIST',
+    column_name='SNOW_SV_NAME',
+    direction='both'
+) }}
