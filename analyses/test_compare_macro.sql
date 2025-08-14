2cd408bdfd00266caa87f1a5883362a812fe2f09
Chuck Frisbie <chuck.frisbie@fishtownanalytics.com>
2025-08-14 21:58:19 +0000

    removed test_compare_macro

--- analyses/test_compare_macro.sql
+++
@@ -1,7 +0,0 @@
-{{ get_model_name_mismatches_sql(
-    database='FRISBIE_DEVELOPMENT',
-    schema='SANDBOX',
-    table_name='SNOW_SV_LIST',
-    column_name='SNOW_SV_NAME',
-    direction='both'
-) }}
